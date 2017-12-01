eeff0f26ccd9099d3ac458f33d09abe254ef48f3
adam <adam@debian-vm>
2017-12-01 20:53:29 +0100

    new folder

---
+++ src/sockethandler.js
@@ -0,0 +1,37 @@
+var webSocket;
+var console;
+function initSockets()
+{
+    webSocket = new WebSocket("ws://localhost:8080/IgiAdventures/igisocket");
+    webSocket.onmessage = function(evt) { onMessage(evt) };
+    webSocket.onopen = function(evt) { onOpen(evt) };
+    webSocket.onclose = function(evt) { onClose(evt) };
+    webSocket.onerror = function(evt) { onError(evt) };
+ }
+
+function onOpen(evt)
+{
+    console = $("#console");
+    recognizer(evt);
+}
+
+function onClose(evt)
+{
+    recognizer(evt);
+}
+
+function onMessage(evt)
+{
+	recognizer(evt);
+}
+
+function onError(evt)
+{
+	recognizer(evt);
+}
+
+function doSend(message)
+{
+	webSocket.send(message);
+}
+initSockets();
